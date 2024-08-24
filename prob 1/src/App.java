8539e6b6e7b73def2f60c208927ab350fdfef0fe
abhiramt <abhiramdevt2024@gmail.com>
2024-08-24 14:53:56 +0530

    swap number

--- prob 1/src/App.java
+++ prob 1/src/App.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
 public class App {
+    // swapping numbers java 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter number 1 : ");
+
+        int numb1= sc.nextInt();
+
+        System.out.println("enter number 2 : ");
+        int numb2 =sc.nextInt();
+        int tm =numb1;
+        numb1 =numb2;
+        numb2=tm;
+
+        System.out.println("number1 :" + numb1);
+        System.out.println("number1 :" + numb2);
+       
     }
 }
